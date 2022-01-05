b26576e1f8294ad3b31e1d14ddf978c031ad99a6
Lets-A-Code <74007285+Lets-A-Code@users.noreply.github.com>
2022-01-05 20:02:59 +0600

    Version 1.0.1 (Added Event Listeners with GLFW)

--- src/main/java/creator/Window.java
+++ src/main/java/creator/Window.java
@@ -13,12 +13,19 @@ public class Window {
     private String title;
     private long glfwWindow;
 
+    private float r, g, b, a;
+    private boolean fadeToBlack = false;
+
     private static Window window;
 
     private Window() {
         this.width = 1920;
         this.height = 1080;
         this.title = "Mario";
+        r = 1;
+        b = 1;
+        g = 1;
+        a = 1;
     }
 
     public static Window get() {
@@ -65,6 +72,11 @@ public class Window {
             throw new RuntimeException("Failed to create the GLFW window");
         }
 
+        glfwSetCursorPosCallback(glfwWindow, MouseListener::mousePosCallback);
+        glfwSetMouseButtonCallback(glfwWindow, MouseListener::mouseButtonCallback);
+        glfwSetScrollCallback(glfwWindow, MouseListener::mouseScrollCallback);
+        glfwSetKeyCallback(glfwWindow, KeyListener::keyCallback);
+
         // Make the OpenGL context current
         glfwMakeContextCurrent(glfwWindow);
 
@@ -87,8 +99,18 @@ public class Window {
             // Poll events
             glfwPollEvents();
 
-            glClearColor(0.5f, 0.5f, 1.0f, 1.0f);
+            glClearColor(r, g, b, a);
             glClear(GL_COLOR_BUFFER_BIT);
+
+            if (fadeToBlack) {
+                r = Math.max(r - 0.01f, 0);
+                g = Math.max(g - 0.01f, 0);
+                b = Math.max(b - 0.01f, 0);
+            }
+
+            if (KeyListener.isKeyPressed(GLFW_KEY_SPACE)) {
+                fadeToBlack = true;
+            }
 
             glfwSwapBuffers(glfwWindow);
         }
