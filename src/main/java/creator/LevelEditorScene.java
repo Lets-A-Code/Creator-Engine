d9ecd97eceadc2d772a616b156901a2a778e5e76
Lets-A-Code <74007285+Lets-A-Code@users.noreply.github.com>
2022-01-11 22:24:28 +0600

    Version 1.1.2 (Spritesheets)

--- src/main/java/creator/LevelEditorScene.java
+++ src/main/java/creator/LevelEditorScene.java
@@ -1,6 +1,7 @@
 package creator;
 
 import components.SpriteRenderer;
+import components.Spritesheet;
 import org.joml.Vector2f;
 import util.AssetPool;
 import static org.lwjgl.glfw.GLFW.*;
@@ -12,35 +13,41 @@ public class LevelEditorScene extends Scene {
 
     @Override
     public void init() {
+        loadResources();
+
         this.camera = new Camera(new Vector2f(-250, 0));
 
+        Spritesheet sprites = AssetPool.getSpritesheet("assets/images/spritesheet.png");
+
         GameObject mario = new GameObject("Mario", new Transform(new Vector2f(100, 100), new Vector2f(256, 256)));
-        mario.addComponent(new SpriteRenderer(AssetPool.getTexture("assets/images/mario.png")));
+        mario.addComponent(new SpriteRenderer(sprites.getSprite(0)));
         this.addGameObjectToScene(mario);
 
         GameObject goomba = new GameObject("Goomba", new Transform(new Vector2f(400, 100), new Vector2f(256, 256)));
-        goomba.addComponent(new SpriteRenderer(AssetPool.getTexture("assets/images/goomba.png")));
+        goomba.addComponent(new SpriteRenderer(sprites.getSprite(15)));
         this.addGameObjectToScene(goomba);
-
-        loadResources();
     }
 
     private void loadResources() {
         AssetPool.getShader("assets/shaders/default.glsl");
+
+        AssetPool.addSpritesheet("assets/images/spritesheet.png",
+                new Spritesheet(AssetPool.getTexture("assets/images/spritesheet.png"),
+                        16, 16, 26, 0));
     }
 
     @Override
     public void update(float dt) {
-        if (KeyListener.isKeyPressed(GLFW_KEY_RIGHT)) {
-            camera.position.x += 250f * dt;
-        } else if (KeyListener.isKeyPressed(GLFW_KEY_LEFT)) {
-            camera.position.x -= 250f * dt;
+        if (KeyListener.isKeyPressed(GLFW_KEY_UP) || KeyListener.isKeyPressed(GLFW_KEY_W)) {
+            camera.position.y += 250f * dt;
+        } else if (KeyListener.isKeyPressed(GLFW_KEY_DOWN) || KeyListener.isKeyPressed(GLFW_KEY_S)) {
+            camera.position.y -= 250f * dt;
         }
 
-        if (KeyListener.isKeyPressed(GLFW_KEY_UP)) {
-            camera.position.y += 250f * dt;
-        } else if (KeyListener.isKeyPressed(GLFW_KEY_DOWN)) {
-            camera.position.y -= 250f * dt;
+        if (KeyListener.isKeyPressed(GLFW_KEY_LEFT) || KeyListener.isKeyPressed(GLFW_KEY_A)) {
+            camera.position.x -= 250f * dt;
+        } else if (KeyListener.isKeyPressed(GLFW_KEY_RIGHT) || KeyListener.isKeyPressed(GLFW_KEY_D)) {
+            camera.position.x += 250f * dt;
         }
 
         for (GameObject go : this.gameObjects) {
--- src/main/java/util/AssetPool.java
+++ src/main/java/util/AssetPool.java
@@ -1,5 +1,6 @@
 package util;
 
+import components.Spritesheet;
 import renderer.Shader;
 import renderer.Texture;
 import java.io.File;
@@ -9,9 +10,11 @@ import java.util.Map;
 public class AssetPool {
     private static Map<String, Shader> shaders = new HashMap<>();
     private static Map<String, Texture> textures = new HashMap<>();
+    private static Map<String, Spritesheet> spritesheets = new HashMap<>();
 
     public static Shader getShader(String resourceName) {
         File file = new File(resourceName);
+
         if (AssetPool.shaders.containsKey(file.getAbsolutePath())) {
             return AssetPool.shaders.get(file.getAbsolutePath());
         } else {
@@ -24,6 +27,7 @@ public class AssetPool {
 
     public static Texture getTexture(String resourceName) {
         File file = new File(resourceName);
+
         if (AssetPool.textures.containsKey(file.getAbsolutePath())) {
             return AssetPool.textures.get(file.getAbsolutePath());
         } else {
@@ -31,5 +35,23 @@ public class AssetPool {
             AssetPool.textures.put(file.getAbsolutePath(), texture);
             return texture;
         }
+    }
+
+    public static void addSpritesheet(String resourceName, Spritesheet spritesheet) {
+        File file = new File(resourceName);
+
+        if (!AssetPool.spritesheets.containsKey(file.getAbsolutePath())) {
+            AssetPool.spritesheets.put(file.getAbsolutePath(), spritesheet);
+        }
+    }
+
+    public static Spritesheet getSpritesheet(String resourceName) {
+        File file = new File(resourceName);
+
+        if (!AssetPool.spritesheets.containsKey(file.getAbsolutePath())) {
+            assert false : "Error: Tried to access spritesheet '" + resourceName + "' and it has not been added to asset pool.";
+        }
+
+        return AssetPool.spritesheets.getOrDefault(file.getAbsolutePath(), null);
     }
 }
